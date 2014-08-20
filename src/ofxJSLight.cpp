31a0917d0c012c4a21bb91ce14cc43de9ef59845
v3ga <julien@v3ga.net>
2014-08-20 14:51:51 +0200

    ofxJSLight implementation

--- src/ofxJSLight.cpp
+++ src/ofxJSLight.cpp
@@ -7,3 +7,42 @@
 //
 
 #include "ofxJSLight.h"
+#include "js/ofxJSLight.cpp"
+
+
+ofxJSLight::ofxJSLight()
+{
+}
+
+ofxJSLight::~ofxJSLight()
+{
+}
+
+
+void ofxJSLight::initialize()
+{
+	ofxJSLight::JSInit	(ofxJSGetContext(), ofxJSGetGlobalObj());
+}
+
+
+void ofxJSLight::destroy()
+{
+	m_light.destroy();
+}
+
+
+void ofxJSLight::enable()
+{
+	m_light.enable();
+}
+
+void ofxJSLight::disable()
+{
+	m_light.disable();
+}
+
+int ofxJSLight::getLightID()
+{
+	return m_light.getLightID();
+}
+
